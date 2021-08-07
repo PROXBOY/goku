816d073bfba5bac7bf960ac9c7ec04c533e88e27
Delux <75972578+angad005@users.noreply.github.com>
2021-08-07 19:39:11 +0530

    Update antiflood.py

--- Goku/modules/antiflood.py
+++ Goku/modules/antiflood.py
@@ -6,10 +6,10 @@ from telegram.error import BadRequest
 from telegram.ext import Filters, MessageHandler, CommandHandler, run_async
 from telegram.utils.helpers import mention_html
 
-from cinderella import dispatcher
-from cinderella.modules.helper_funcs.chat_status import is_user_admin, user_admin, can_restrict, can_delete
-from cinderella.modules.log_channel import loggable
-from cinderella.modules.sql import antiflood_sql as sql
+from Goku import dispatcher
+from Goku.modules.helper_funcs.chat_status import is_user_admin, user_admin, can_restrict, can_delete
+from Goku.modules.log_channel import loggable
+from Goku.modules.sql import antiflood_sql as sql
 
 FLOOD_GROUP = 3
 
