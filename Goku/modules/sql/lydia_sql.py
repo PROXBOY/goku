e8763699eb308ce17f1e4941891eefd954dcc0c9
Delux <75972578+angad005@users.noreply.github.com>
2021-08-07 18:20:22 +0530

    Update lydia_sql.py

--- Goku/modules/sql/lydia_sql.py
+++ Goku/modules/sql/lydia_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String
 
-from cinderella.modules.sql import BASE, SESSION
+from Goku.modules.sql import BASE, SESSION
 
 
 class ChatbotChats(BASE):
