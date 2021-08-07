5ef58a1326650f341fd6293f8d3d433bc781952b
Delux <75972578+angad005@users.noreply.github.com>
2021-08-07 18:11:38 +0530

    Update cleaner_sql.py

--- Goku/modules/sql/cleaner_sql.py
+++ Goku/modules/sql/cleaner_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean
 
-from cinderella.modules.sql import BASE, SESSION
+from Goku.modules.sql import BASE, SESSION
 
 
 class CleanerBlueTextChatSettings(BASE):
