21490da774901b1833eeb22a734bcf067012ed8a
Delux <75972578+angad005@users.noreply.github.com>
2021-08-07 18:19:41 +0530

    Update log_channel_sql.py

--- Goku/modules/sql/log_channel_sql.py
+++ Goku/modules/sql/log_channel_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, func, distinct
 
-from cinderella.modules.sql import BASE, SESSION
+from Goku.modules.sql import BASE, SESSION
 
 
 class GroupLogs(BASE):
