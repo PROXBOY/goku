b02272a21f2405cdd7202276a0005f725a34328d
Delux <75972578+angad005@users.noreply.github.com>
2021-08-07 20:36:35 +0530

    Update reactions.py

--- Goku/modules/reactions.py
+++ Goku/modules/reactions.py
@@ -3,8 +3,8 @@ import random
 from telegram import Bot, Update
 from telegram.ext import run_async
 
-from cinderella import dispatcher
-from cinderella.modules.disable import DisableAbleCommandHandler
+from Goku import dispatcher
+from Goku.modules.disable import DisableAbleCommandHandler
 
 
 reactions = [
