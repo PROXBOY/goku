47f0a4213fb959f3fd682aa3790eb5951ad807a6
Delux <75972578+angad005@users.noreply.github.com>
2021-08-07 18:15:39 +0530

    Update feds_sql.py

--- Goku/modules/sql/feds_sql.py
+++ Goku/modules/sql/feds_sql.py
@@ -3,8 +3,8 @@ import threading
 from sqlalchemy import Column, String, UnicodeText, func, distinct, Integer, Boolean
 from telegram.error import BadRequest, TelegramError, Unauthorized
 
-from cinderella import dispatcher
-from cinderella.modules.sql import SESSION, BASE
+from Goku import dispatcher
+from Goku.modules.sql import SESSION, BASE
 
 
 class Federations(BASE):
