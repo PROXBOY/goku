ed05be1c2c51ad757bb68dbec854a9a9966c4cc8
Delux <75972578+angad005@users.noreply.github.com>
2021-08-07 18:13:42 +0530

    Update cust_filters_sql.py

--- Goku/modules/sql/cust_filters_sql.py
+++ Goku/modules/sql/cust_filters_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
 
-from cinderella.modules.sql import BASE, SESSION
+from Goku.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
