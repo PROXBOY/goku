02be184885257259f816bac01ec34afdda03a6fd
Delux <75972578+angad005@users.noreply.github.com>
2021-08-07 18:10:02 +0530

    Update blacklistusers_sql.py

--- Goku/modules/sql/blacklistusers_sql.py
+++ Goku/modules/sql/blacklistusers_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText
 
-from cinderella.modules.sql import BASE, SESSION
+from Goku.modules.sql import BASE, SESSION
 
 
 class BlacklistUsers(BASE):
