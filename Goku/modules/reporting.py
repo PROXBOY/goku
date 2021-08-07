8d85118e97cb40a3a4718c750c7b2ddb0d5f13af
Delux <75972578+angad005@users.noreply.github.com>
2021-08-07 20:33:45 +0530

    Update reporting.py

--- Goku/modules/reporting.py
+++ Goku/modules/reporting.py
@@ -7,10 +7,10 @@ from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CommandHandler, RegexHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from cinderella import dispatcher, LOGGER, DEV_USERS, WHITELIST_USERS
-from cinderella.modules.helper_funcs.chat_status import user_not_admin, user_admin
-from cinderella.modules.log_channel import loggable
-from cinderella.modules.sql import reporting_sql as sql
+from Goku import dispatcher, LOGGER, DEV_USERS, WHITELIST_USERS
+from Goku.modules.helper_funcs.chat_status import user_not_admin, user_admin
+from Goku.modules.log_channel import loggable
+from Goku.modules.sql import reporting_sql as sql
 
 REPORT_GROUP = 5
 REPORT_IMMUNE_USERS = DEV_USERS + WHITELIST_USERS
