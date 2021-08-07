cf012646e76441b534d02b17163bc86aa77a965b
Delux <75972578+angad005@users.noreply.github.com>
2021-08-07 20:22:10 +0530

    Update special.py

--- Goku/modules/special.py
+++ Goku/modules/special.py
@@ -10,11 +10,11 @@ from telegram.ext import MessageHandler, Filters, CommandHandler
 from telegram.ext.dispatcher import run_async
 from telegram.utils.helpers import escape_markdown
 from html import escape
-from cinderella.modules.helper_funcs.chat_status import is_user_ban_protected, bot_admin
+from Goku.modules.helper_funcs.chat_status import is_user_ban_protected, bot_admin
 
-import cinderella.modules.sql.users_sql as sql
-from cinderella import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, LOGGER
-from cinderella.modules.helper_funcs.filters import CustomFilters
+import Goku.modules.sql.users_sql as sql
+from Goku import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, LOGGER
+from Goku.modules.helper_funcs.filters import CustomFilters
 
 USERS_GROUP = 4
 
