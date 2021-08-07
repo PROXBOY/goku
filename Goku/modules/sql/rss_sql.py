0c0687442a128643e30488231f9008e2b04e9929
Delux <75972578+angad005@users.noreply.github.com>
2021-08-07 18:22:32 +0530

    Update rss_sql.py

--- Goku/modules/sql/rss_sql.py
+++ Goku/modules/sql/rss_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Integer
 
-from cinderella.modules.sql import BASE, SESSION
+from Goku.modules.sql import BASE, SESSION
 
 
 class RSS(BASE):
