245d57b53e9ede6445b62357eb1e6c73a561928e
Delux <75972578+angad005@users.noreply.github.com>
2021-08-07 22:26:15 +0530

    Update getlink.py

--- Goku/modules/getlink.py
+++ Goku/modules/getlink.py
@@ -4,10 +4,10 @@ from telegram import Update, Bot, Chat, Message, User
 from telegram.error import BadRequest
 from telegram.ext import CommandHandler, Filters
 from telegram.ext.dispatcher import run_async
-from cinderella.modules.helper_funcs.chat_status import bot_admin
-from cinderella.modules.helper_funcs.filters import CustomFilters
+from Goku.modules.helper_funcs.chat_status import bot_admin
+from Goku.modules.helper_funcs.filters import CustomFilters
 
-from cinderella import dispatcher
+from Goku import dispatcher
 import random, re
 
 @run_async
