c56ebb0e9d1ebc4a1b570ad829064206cb167919
Delux <75972578+angad005@users.noreply.github.com>
2021-08-07 20:32:26 +0530

    Update reverseimage.py

--- Goku/modules/reverseimage.py
+++ Goku/modules/reverseimage.py
@@ -10,9 +10,9 @@ from typing import List
 from telegram import ParseMode, InputMediaPhoto, Update, Bot, TelegramError
 from telegram.ext import run_async
 
-from cinderella import dispatcher
+from Goku import dispatcher
 
-from cinderella.modules.disable import DisableAbleCommandHandler
+from Goku.modules.disable import DisableAbleCommandHandler
 
 
 opener = urllib.request.build_opener()
