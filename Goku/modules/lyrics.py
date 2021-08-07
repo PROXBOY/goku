9a171964226a7cc0eeb2c874669cfb6e62490ac5
Delux <75972578+angad005@users.noreply.github.com>
2021-08-07 20:46:50 +0530

    Update lyrics.py

--- Goku/modules/lyrics.py
+++ Goku/modules/lyrics.py
@@ -3,8 +3,8 @@ from tswift import Song
 from telegram import Bot, Update, Message, Chat
 from telegram.ext import run_async
 
-from cinderella import dispatcher
-from cinderella.modules.disable import DisableAbleCommandHandler
+from Goku import dispatcher
+from Goku.modules.disable import DisableAbleCommandHandler
 
 
 @run_async
