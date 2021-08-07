1df16a981bc24ae5f8052f91eecdbf697ac95932
Delux <75972578+angad005@users.noreply.github.com>
2021-08-07 18:23:20 +0530

    Update userinfo_sql.py

--- Goku/modules/sql/userinfo_sql.py
+++ Goku/modules/sql/userinfo_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, Integer, UnicodeText
 
-from cinderella.modules.sql import SESSION, BASE
+from Goku.modules.sql import SESSION, BASE
 
 
 class UserInfo(BASE):
