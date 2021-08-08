7317632b0d76ea8b2c162e9d9d7c9643e9c0c7ca
Delux <75972578+angad005@users.noreply.github.com>
2021-08-08 10:55:24 +0530

    Update covid_tracker.py

--- Goku/modules/covid_tracker.py
+++ Goku/modules/covid_tracker.py
@@ -4,8 +4,8 @@ from urllib.request import urlopen
 from telegram import ParseMode, Update, Bot, Chat
 from telegram.ext import CommandHandler, MessageHandler, BaseFilter, run_async
 
-from cinderella import dispatcher
-from cinderella.modules.disable import DisableAbleCommandHandler
+from Goku import dispatcher
+from Goku.modules.disable import DisableAbleCommandHandler
 from requests import get
 
 def covindia(bot: Bot, update: Update):
