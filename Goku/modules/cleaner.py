0e12bf67f3dccc3c8972823907d45e7fb50c066d
Delux <75972578+angad005@users.noreply.github.com>
2021-08-08 10:57:35 +0530

    Update cleaner.py

--- Goku/modules/cleaner.py
+++ Goku/modules/cleaner.py
@@ -5,10 +5,10 @@ from typing import List
 from telegram import Bot, Update, ParseMode
 from telegram.ext import CommandHandler, MessageHandler, Filters, run_async
 
-from cinderella import ALLOW_EXCL, dispatcher, CustomCommandHandler
-from cinderella.modules.disable import DisableAbleCommandHandler
-from cinderella.modules.helper_funcs.chat_status import user_admin, bot_can_delete, dev_plus, connection_status
-from cinderella.modules.sql import cleaner_sql as sql
+from Goku import ALLOW_EXCL, dispatcher, CustomCommandHandler
+from Goku.modules.disable import DisableAbleCommandHandler
+from Goku.modules.helper_funcs.chat_status import user_admin, bot_can_delete, dev_plus, connection_status
+from Goku.modules.sql import cleaner_sql as sql
 
 if ALLOW_EXCL:
     CMD_STARTERS = ('/', '!')
